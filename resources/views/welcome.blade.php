3c8845836faa52eb64e831107890caf805ee60ef
Rey Jhon Baquirin <reyjhonbaquirin@yahoo.com>
2020-11-30 12:47:25 +0800

    Update page open graph meta data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>LISA</title>
+  <title>LALISA</title>
   <meta content="Lisa lang sapat na." name="description">
   <meta content="https://lisaloves.me" name="url">
   <meta name="image" content="https://lisaloves.me/assets/img/lisa.jpg" />
   <meta content="Lisa manoban Blackpink Lisa lang sapat na blinks" name="keywords">
-  <meta property="og:title" content="The Rock" />
+  <meta property="og:title" content="LALISA" />
   <meta property="og:type" content="website" />
   <meta property="og:description" content="Lisa lang sapat na" />
   <meta property="og:url" content="https://lisaloves.me" />
