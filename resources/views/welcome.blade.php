931d2798354af2a53c4f00df4342da0126ea47e3
Rey Jhon Baquirin <reyjhonbaquirin@yahoo.com>
2020-11-30 12:00:06 +0800

    Update initial page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Personal Bootstrap Template</title>
+  <title>LISA</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="/assets/img/favicon.png" rel="icon">
+  <link href="/assets/img/favicon.ico" rel="icon">
   <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
@@ -44,7 +44,7 @@
       <h1><a href="/">Lisa loves me</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="mr-auto"><img src="/assets/img/logo.png" alt="" class="img-fluid"></a> -->
-      <h2>Akin lang ang <span>asawa</span> ko.</h2>
+      <h2>Lisa lang <span>sapat</span> na.</h2>
 
 
       <div class="social-links">
