7c9f40dfe23037082fcbf685677245174835e1c6
Rey Jhon Baquirin <reyjhonbaquirin@yahoo.com>
2020-11-30 13:29:59 +0800

    Update page social links icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <a href="https://twitter.com/BLACKPINK" class="twitter"><i class="icofont-twitter"></i></a>
         <a href="https://www.facebook.com/blackpinkxxJenSooLisChaeng/" class="facebook"><i class="icofont-facebook"></i></a>
         <a href="https://www.instagram.com/blackpinkofficial/" class="instagram"><i class="icofont-instagram"></i></a>
-       
+        <a href="https://www.youtube.com/channel/UCOmHUn--16B90oW2L6FRR3A" class="youtube"><i class="icofont-youtube-play"></i></a>
       </div>
 
     </div>
